4abcf8ad2df2741904b9699efe3cd56051b3bf61
salem <chu1070y@naver.com>
2018-12-12 20:06:43 +0900

    superadmin menuadd

--- src/main/java/ga/eatup/partner/controller/SuperadminController.java
+++ src/main/java/ga/eatup/partner/controller/SuperadminController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import ga.eatup.partner.service.SuperadminService;
 import ga.eatup.user.domain.MenuVO;
@@ -23,21 +24,29 @@ public class SuperadminController {
 	@Setter(onMethod_ = @Autowired)
 	private SuperadminService service;
 	
+	private static int sno = 0;
+	
 	@PostMapping("menuadd")
-	public void menuAdd(MenuVO vo) {
+	public String menuAdd(MenuVO vo, RedirectAttributes redirect) {
 		log.info("menu add......................");
+		vo.setSno(sno);
+		vo.getSno();
+		log.info("menu sno : " + vo.getSno());
+		log.info("결과는? "+ service.menuAdd(vo));
 		
-		log.info("결과는? "+service.menuAdd(vo));
-		
+		return "redirect:/partner/superAdmin";
 	}
 	
 	@GetMapping("/{sname}")
 	@ResponseBody
 	public ResponseEntity<Integer> searchSno(@PathVariable("sname") String sname){
 		log.info("searchSno get.....");
-		log.info(""+service.searchSno(sname));
 		
-		return new ResponseEntity<>(service.searchSno(sname),HttpStatus.OK);
+		sno = service.searchSno(sname);
+		
+		log.info(""+sno);
+		
+		return new ResponseEntity<>(sno,HttpStatus.OK);
 	}
 
 }
