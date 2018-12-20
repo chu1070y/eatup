f5d23c930539a953afc3d6512b76798c895d595e
salem <chu1070y@naver.com>
2018-12-20 12:35:57 +0900

    시큐리티 권한

--- src/main/java/ga/eatup/config/PartnerSecurityConfig.java
+++ src/main/java/ga/eatup/config/PartnerSecurityConfig.java
@@ -4,18 +4,20 @@ import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import lombok.extern.java.Log;
 
 @Log
+@Configuration
 @EnableWebSecurity
 @Order(value= 1)
 public class PartnerSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -50,11 +52,9 @@ public class PartnerSecurityConfig extends WebSecurityConfigurerAdapter {
 				.and()
 			.formLogin()
 				.loginPage("/partner/login/customLogin");
-		
-		
-
 	}
 	
+
 	
 	@Bean
 	public PasswordEncoder passwordEncoder() {
@@ -66,5 +66,7 @@ public class PartnerSecurityConfig extends WebSecurityConfigurerAdapter {
 		
 		return new CustomUserDetailsService();
 	}
+	
+	
 		
 }
