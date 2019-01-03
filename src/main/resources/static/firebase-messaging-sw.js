424ef03e0c1123618ba6556d894d53bc03ff29ba
salem <chu1070y@naver.com>
2019-01-03 15:08:50 +0900

    index 오류 조금 해결

--- src/main/resources/static/firebase-messaging-sw.js
+++ src/main/resources/static/firebase-messaging-sw.js
@@ -10,14 +10,16 @@ var config = {
   storageBucket: "gorany-df5bd.appspot.com",
   messagingSenderId: "551706831448"
 };
+
 firebase.initializeApp(config);
 
 const messaging = firebase.messaging();
+
 messaging.setBackgroundMessageHandler(function(payload){
-	
+
 	const title = "고라니 서비스";
 	const options = {
-			body: payload.notification.body
+			body: "dfsafd"
 	};
 	
 	return self.registration.showNotification(title,options);
