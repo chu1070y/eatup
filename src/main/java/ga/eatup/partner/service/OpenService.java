986af2f59aa2ee4095bdadc1843e51be751b9088
Munjiin <answldls9401@hanmail.net>
2019-01-04 11:44:37 +0900

    온오프 수정이 된거 같은ㄷ ㅔ......ㅇ..........

--- src/main/java/ga/eatup/partner/service/OpenService.java
+++ src/main/java/ga/eatup/partner/service/OpenService.java
@@ -4,11 +4,11 @@ import ga.eatup.partner.domain.StoreVO;
 
 public interface OpenService {
 	
-	/*public int updateX(StoreVO vo);
+	//public int updateX(StoreVO vo);
 	
-	public int updateO(StoreVO vo);*/
+	//public int updateO(StoreVO vo);
 	
-public int updateX(int vo);
+	public int updateX(int vo);
 	
 	public int updateO(int vo);
 
